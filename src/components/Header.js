dabfcd0b850761d44f78d43d3299986b404305b0
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-03-20 19:11:01 +0000

    Complete UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,31 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from 'prop-types';
+import Button from './Button';
 
-const Header = ({ title }) => {
-
-    const onClick = () => {
-        console.log('Click')
-    }
-
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <Button color='green' text='Add' 
-            onClick={onClick} />
-        </header>
-
-    )
-}
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? 'red' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-  }
+  title: 'Task Tracker',
+};
 
-  Header.propTypes = {
-      title: PropTypes.string.isRequired,
-  }
-
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 // CSS in JS
 //   const headingStyle = {
-//     color: 'red', 
+//     color: 'red',
 //     backgroundColor: 'black'
 //   }
-  
-export default Header
+
+export default Header;
